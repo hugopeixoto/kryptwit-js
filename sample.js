7551a6b2798d0a6e87793a3e9412c42339eb3d52
Hugo Peixoto <hugo.peixoto@gmail.com>
2013-03-25 00:24:35 +0000

    Adds length to debugging information

--- sample.js
+++ sample.js
@@ -1,12 +1,12 @@
 
 var username  = "hugopeixoto";
 var password  = "zomgpwd";
-var plaintext = "so kawaii";
-print("input :" + plaintext)
+var plaintext = "so kawai";
+print("input :" + plaintext + " (" + plaintext.length + ")");
 
 var ciphertext = status_update_encrypt(plaintext, username, password);
-print("cipher:" + ciphertext);
+print("cipher:" + ciphertext + " (" + ciphertext.length + ")");
 
 var plaintext2 = status_update_decrypt(ciphertext, username, password);
-print("plain :" + plaintext2);
+print("plain :" + plaintext2 + " (" + plaintext2.length + ")");
 
